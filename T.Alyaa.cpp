22e67e7b488f5a0432b22b22c61e1f26b47f693c
Alyaa Azzahra <alyaazzahra664@gmail.com>
2024-06-25 09:57:05 +0700

    Add files via upload

---
+++ T.Alyaa.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+void inorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+void preorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    cout << root->data << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+void postorderTraversal(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    TreeNode* root = new TreeNode{25, NULL, NULL};
+    root->left = new TreeNode{15, NULL, NULL};
+    root->right = new TreeNode{20, NULL, NULL};
+
+    cout << "Inorder traversal: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    cout << "Preorder traversal: ";
+    preorderTraversal(root);
+    cout << endl;
+
+    cout << "Postorder traversal: ";
+    postorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
+
